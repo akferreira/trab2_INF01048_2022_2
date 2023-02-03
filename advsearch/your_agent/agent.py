0d88c6ac344ee3cc28215eccb3058c2e123f585a
akferreira <49925514+akferreira@users.noreply.github.com>
2023-02-03 13:52:45 -0300

    Versão funcional do player agent

--- advsearch/your_agent/agent.py
+++ advsearch/your_agent/agent.py
@@ -10,14 +10,114 @@ from ..othello.gamestate import GameState
 # do seu agente.
 
 
-def make_move(state: GameState) -> Tuple[int, int]:
-    """
-    Returns an Othello move
-    :param state: state to make the move
-    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
-    """
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada com as pretas.
-    # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+from math import sqrt,log
+
+from queue import Empty as qEmpty
+from collections import deque
+from time import sleep,time
+from threading import Thread
+
+class Nodo():
+  C = 1
+  count = 0 #variável de debug. Utilizada para saber o número total de nodos visitados durante a execução do código
+
+  def __init__(self,gamestate,custo,jogada,pai = None):
+    self.gamestate = gamestate
+    self.custo = custo  #quantidade de movimentos necessários para atingir o estado
+    self.pai = pai
+    self.vitorias = 0
+    self.visitas = 0
+    self.jogada = jogada #qual a jogada é realizada no turno correspondente ao nodo.
+    self.filhos = []
+
+
+
+  def verificar_vitoria(self,ganhador = None):
+    if(self.gamestate.is_terminal()):
+      if(self.gamestate.winner() == self.pai.gamestate.player):
+        self.vitorias += 1
+      return self.gamestate.winner()
+    
+    else:
+      try:
+        if(ganhador == self.pai.gamestate.player):
+          self.vitorias += 1
+      except AttributeError:
+        pass
+
+  def UCB(self):
+    if(self.pai is None or self.visitas == 0):
+      return 1
+
+
+    return (self.vitorias/self.visitas + self.C * sqrt(log(self.pai.visitas)/self.visitas ))
+
+  def criar_tabuleiro(self):
+    if(self.jogada is not None):
+      next_gamestate = self.pai.gamestate.next_state(self.jogada)
+      self.gamestate = next_gamestate
+      return
+
+  def gerar_filhos(self):
+    if(self.gamestate.is_terminal() == False and not self.filhos):
+      jogadas = self.gamestate.legal_moves()
+
+      for jogada in jogadas:
+        
+        self.filhos.append(Nodo(None,self.custo+1,jogada,self))
+
+  def escolhe_melhor_filho(self):
+    if(self.filhos):
+      filhos = sorted(self.filhos, key = Nodo.UCB, reverse = True)
+      return filhos[0]
+
+  def visitar_nodo(self):
+    self.visitas += 1
+    Nodo.count += 1
+    self.criar_tabuleiro()
+    self.gerar_filhos()
+
+def explorar_arvore(nodo):
+  nodo.visitar_nodo()
+  next_nodo = nodo.escolhe_melhor_filho()
+
+  if(next_nodo is not None):
+    ganhador = explorar_arvore(next_nodo)
+    next_nodo.verificar_vitoria(ganhador)
+    return ganhador
+
+  #nodo terminal
+  return nodo.verificar_vitoria()
+
+def make_move(estado_atual):
+  arvore = Nodo(estado_atual,0,None)
+  start = time()
+  now = time()
+
+  while(now - start < 4.9):
+    explorar_arvore(arvore)
+    now = time()
+
+
+  # print(arvore.visitas)
+  best = arvore.escolhe_melhor_filho()
+ # print(best.jogada)
+
+
+ # for filho in arvore.filhos:
+   # print(f"{filho.vitorias}/{filho.visitas}")
+   # for x in filho.filhos:
+   #   print(f"\t {x.vitorias}/{x.visitas}")
+
+ # print(Nodo.count)
+  return best.jogada
+
+
+
+#estado = GameState(tabuleiro,'B')
+#arvore = Nodo(estado,0,None)
+#print(arvore.gamestate.winner())
+
+#make_move(estado)
+
 
